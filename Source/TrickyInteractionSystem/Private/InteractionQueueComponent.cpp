f87865d7284f28258fed805d3cf782309fa4f1a7
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-04-29 22:59:40 +0300

    Create InteractionQueueComponent

---
+++ Source/TrickyInteractionSystem/Private/InteractionQueueComponent.cpp
@@ -0,0 +1,179 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "InteractionQueueComponent.h"
+
+#include "TrickyInteractionInterface.h"
+
+UInteractionQueueComponent::UInteractionQueueComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UInteractionQueueComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+
+void UInteractionQueueComponent::TickComponent(float DeltaTime,
+                                               ELevelTick TickType,
+                                               FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+
+bool UInteractionQueueComponent::AddToInteractionQueue(AActor* InteractiveActor)
+{
+	if (!IsActorInteractive(InteractiveActor) || IsInInteractionQueue(InteractiveActor))
+	{
+		return false;
+	}
+
+	InteractionQueue.Emplace(InteractiveActor);
+	return true;
+}
+
+bool UInteractionQueueComponent::RemoveFromInteractionQueue(AActor* InteractiveActor)
+{
+	if (!IsActorInteractive(InteractiveActor) || !IsInInteractionQueue(InteractiveActor) || IsInteractionQueueEmpty())
+	{
+		return false;
+	}
+
+	return InteractionQueue.RemoveSingle(InteractiveActor);
+}
+
+
+bool UInteractionQueueComponent::IsInInteractionQueue(AActor* InteractiveActor)
+{
+	if (!IsActorInteractive(InteractiveActor))
+	{
+		return false;
+	}
+
+	return InteractionQueue.Contains(InteractiveActor);
+}
+
+bool UInteractionQueueComponent::StartInteraction()
+{
+	AActor* Interactor = GetOwner();
+
+	if (IsInteractionQueueEmpty() || !IsValid(Interactor))
+	{
+		return false;
+	}
+
+	AActor* InteractiveActor = InteractionQueue[0];
+
+	if (!IsValid(InteractiveActor))
+	{
+		return false;
+	}
+
+	const bool bIsSuccess = ITrickyInteractionInterface::Execute_StartInteraction(InteractiveActor, Interactor);
+
+	if (bIsSuccess)
+	{
+		FInteractionData InteractionData;
+		GetActorInteractionData(InteractiveActor, InteractionData);
+		OnInteractionStarted.Broadcast(this, InteractiveActor, InteractionData);
+	}
+
+	return bIsSuccess;
+}
+
+bool UInteractionQueueComponent::FinishInteraction()
+{
+	AActor* Interactor = GetOwner();
+
+	if (IsInteractionQueueEmpty() || !IsValid(Interactor))
+	{
+		return false;
+	}
+
+	AActor* InteractiveActor = InteractionQueue[0];
+
+	if (!IsValid(InteractiveActor))
+	{
+		return false;
+	}
+
+	const bool bIsSuccess = ITrickyInteractionInterface::Execute_FinishInteraction(InteractiveActor, Interactor);
+	OnInteractionFinished.Broadcast(this, InteractiveActor, bIsSuccess);
+	return bIsSuccess;
+}
+
+bool UInteractionQueueComponent::InterruptInteraction()
+{
+	AActor* Interruptor = GetOwner();
+
+	if (IsInteractionQueueEmpty() || !IsValid(Interruptor))
+	{
+		return false;
+	}
+
+	AActor* InteractiveActor = InteractionQueue[0];
+
+	if (!IsValid(InteractiveActor))
+	{
+		return false;
+	}
+
+	const bool bIsSuccess = ITrickyInteractionInterface::Execute_InterruptInteraction(InteractiveActor, Interruptor);
+
+	if (bIsSuccess)
+	{
+		OnInteractionInterrupted.Broadcast(this, InteractiveActor);
+	}
+
+	return bIsSuccess;
+}
+
+bool UInteractionQueueComponent::ForceInteraction()
+{
+	AActor* Interactor = GetOwner();
+
+	if (IsInteractionQueueEmpty() || !IsValid(Interactor))
+	{
+		return false;
+	}
+
+	AActor* InteractiveActor = InteractionQueue[0];
+
+	if (!IsValid(InteractiveActor))
+	{
+		return false;
+	}
+
+	const bool bIsSuccess = ITrickyInteractionInterface::Execute_ForceInteraction(InteractiveActor, Interactor);
+
+	if (bIsSuccess)
+	{
+		OnInteract.Broadcast(this, InteractiveActor, bIsSuccess);
+	}
+
+	return bIsSuccess;
+}
+
+bool UInteractionQueueComponent::IsActorInteractive(AActor* Actor)
+{
+	if (!IsValid(Actor) || !Actor->Implements<UTrickyInteractionInterface>())
+	{
+		return false;
+	}
+
+	FInteractionData InteractionData;
+	return ITrickyInteractionInterface::Execute_GetInteractionData(Actor, InteractionData);
+}
+
+bool UInteractionQueueComponent::GetActorInteractionData(AActor* InteractiveActor, FInteractionData& InteractionData)
+{
+	if (!IsValid(InteractiveActor) || !InteractiveActor->Implements<UTrickyInteractionInterface>())
+	{
+		return false;
+	}
+
+	return ITrickyInteractionInterface::Execute_GetInteractionData(InteractiveActor, InteractionData);
+}
---
+++ Source/TrickyInteractionSystem/Public/InteractionQueueComponent.h
@@ -0,0 +1,110 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InteractionQueueComponent.generated.h"
+
+struct FInteractionData;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnActorAddedToInteractionQueueDynamicSignature,
+                                               UInteractionQueueComponent*, Component,
+                                               AActor*, InteractiveActor,
+                                               const FInteractionData&, InteractionData);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnActorRemovedFromInteractionQueueDynamicSignature,
+                                             UInteractionQueueComponent*, Component,
+                                             AActor*, InteractiveActor);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnInteractionStartedDynamicSignature,
+                                               UInteractionQueueComponent*, Component,
+                                               AActor*, InteractiveActor,
+                                               const FInteractionData&, InteractionData);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnInteractionFinishedDynamicSignature,
+                                               UInteractionQueueComponent*, Component,
+                                               AActor*, InteractievActor,
+                                               bool, bIsSuccessful);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnInteractionInterruptedDynamicSignature,
+                                             UInteractionQueueComponent*, Component,
+                                             AActor*, InteractiveActor);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnInteractDynamicSignature,
+                                               UInteractionQueueComponent*, Component,
+                                               AActor*, InteractiveActor,
+                                               bool, bIsSuccessful);
+
+UCLASS(ClassGroup=(TrickyInteractionSystem), meta=(BlueprintSpawnableComponent))
+class TRICKYINTERACTIONSYSTEM_API UInteractionQueueComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UInteractionQueueComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	virtual void TickComponent(float DeltaTime,
+	                           ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BuleprintAssignable, Category="InteractionQueue")
+	FOnActorAddedToInteractionQueueDynamicSignature OnActorAddedToInteractionQueue;
+
+	UPROPERTY(BuleprintAssignable, Category="InteractionQueue")
+	FOnActorRemovedFromInteractionQueueDynamicSignature OnActorRemovedFromInteractionQueue;
+
+	UPROPERTY(BlueprintAssignable, Category="InteractionQueue")
+	FOnInteractionStartedDynamicSignature OnInteractionStarted;
+
+	UPROPERTY(BlueprintAssignable, Category="InteractionQueue")
+	FOnInteractionFinishedDynamicSignature OnInteractionFinished;
+
+	UPROPERTY(BlueprintAssignable, Category="InteractionQueue")
+	FOnInteractionInterruptedDynamicSignature OnInteractionInterrupted;
+
+	UPROPERTY(BlueprintAssignable, Category="InteractionQueue")
+	FOnInteractDynamicSignature OnInteract;
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool AddToInteractionQueue(AActor* InteractiveActor);
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool RemoveFromInteractionQueue(AActor* InteractiveActor);
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool IsInInteractionQueue(AActor* InteractiveActor);
+
+	UFUNCTION(BlueprintGetter)
+	TArray<AActor*> GetInteractionQueue() const
+	{
+		return InteractionQueue;
+	};
+
+	UFUNCTION(BlueprintPure, Category="InteractionQueue")
+	bool IsInteractionQueueEmpty() const { return InteractionQueue.IsEmpty(); };
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool StartInteraction();
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool FinishInteraction();
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool InterruptInteraction();
+
+	UFUNCTION(BlueprintCallable, Category="InteractionQueue")
+	bool ForceInteraction();
+
+private:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="InteractionQueue")
+	TArray<AActor*> InteractionQueue;
+
+	static bool IsActorInteractive(AActor* Actor);
+
+	static bool GetActorInteractionData(AActor* InteractiveActor, FInteractionData& InteractionData);
+};
