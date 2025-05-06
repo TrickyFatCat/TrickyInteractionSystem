8b0f841a6a07d79d1afeafab80c99aa42099436c
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-05-06 20:04:05 +0300

    Create TrickyInteractionLibrary

---
+++ Source/TrickyInteractionSystem/Private/TrickyInteractionLibrary.cpp
@@ -0,0 +1,51 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyInteractionLibrary.h"
+
+#include "TrickyInteractionInterface.h"
+
+bool UTrickyInteractionLibrary::IsActorInteractive(const AActor* Actor)
+{
+	if (!IsValid(Actor) || !Actor->Implements<UTrickyInteractionInterface>())
+	{
+		return false;
+	}
+
+	FInteractionData InteractionData;
+	return GetActorInteractionData(Actor, InteractionData);
+}
+
+bool UTrickyInteractionLibrary::GetActorInteractionData(const AActor* Actor, FInteractionData& InteractionData)
+{
+	if (!IsValid(Actor) && !Actor->Implements<UTrickyInteractionInterface>())
+	{
+		return false;
+	}
+
+	const UClass* Class = Actor->GetClass();
+
+	FProperty* Property = Class->FindPropertyByName(TEXT("InteractionData"));
+
+	if (!Property)
+	{
+		return false;
+	}
+
+	const FStructProperty* StructProperty = CastField<FStructProperty>(Property);
+
+	if (!StructProperty)
+	{
+		return false;
+	}
+
+	const FName StructName = StructProperty->Struct->GetFName();
+
+	if (StructName != FName("InteractionData"))
+	{
+		return false;
+	}
+
+	InteractionData = *StructProperty->ContainerPtrToValuePtr<FInteractionData>(Actor);
+	return true;
+}
---
+++ Source/TrickyInteractionSystem/Public/TrickyInteractionLibrary.h
@@ -0,0 +1,28 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TrickyInteractionLibrary.generated.h"
+
+struct FInteractionData;
+/**
+ * 
+ */
+UCLASS()
+class TRICKYINTERACTIONSYSTEM_API UTrickyInteractionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category="TrickyInteraction", meta=(WorldContext="Actor"))
+	static bool IsActorInteractive(const AActor* Actor);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyInteraction", meta=(WorldContext="Actor"))
+	static bool GetActorInteractionData(const AActor* Actor,
+	                                    FInteractionData& InteractionData);
+
+private:
+	const FString InteractionDataName = "InteractionData";
+};
